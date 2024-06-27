3915b4f681d5947482a0479abe04962ad20ece2a
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-06-27 19:02:56 +0900

    폴더구조 변경

---
+++ hackathon/src/Dev/Employment/Components/NavBar/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <header>
+            
+        </header>
+    );
+};
+
+export default Nav;
---
+++ hackathon/src/Dev/Employment/Pages/Onboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const onboard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default onboard;
